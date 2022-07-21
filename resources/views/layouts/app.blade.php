3f50a3d4aa9ce1867056485282e71756ef3d43f4
CUjuanpablo <cuaj000726@gmail.com>
2022-07-21 18:41:24 -0500

    auth resources

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,91 +1,33 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
     <head>
-
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        {{-- favIcon --}}
-        <link rel="icon" type="image/x-icon" href="{{ asset('/img/favicon/favicon.ico')}}" />
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-            href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-            rel="stylesheet" />
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('/css/demo.css')}}" />
-        <link rel="stylesheet" href="{{ asset('/css/core.css')}}" class="template-customizer-core-css" />
-        <link rel="stylesheet" href="{{ asset('/css/theme-default.css')}}" class="template-customizer-theme-css" />
-
-        <!-- estilos para los iconos -->
-        <link rel="stylesheet" href="{{ asset('/fonts/boxicons.css')}}" />
 
         <!-- Scripts -->
-        <script src="{{ asset('/js/helpers.js')}}"></script>
-        <script src="{{ asset('/js/config.js')}}"></script>
-        <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-        @yield('cdns')
-
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
 
-    <body>
-        <div class="layout-wrapper layout-content-navbar">
-            <div class="layout-container">
-                @include('layouts.asideNav')
-                <div class="layout-page">
-                    @include('layouts.navTop')
-                    <div class="content-wrapper">
-                        <div class="container-xxl flex-grow-1 container-p-y">
-                            <main>
-                                @yield('content')
-                            </main>
-                        </div>
-                    </div>
-                    @include('layouts.footer')
+            <!-- Page Heading -->
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
                 </div>
-            </div>
+            </header>
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
         </div>
     </body>
-
-
-<script src="{{ asset('/js/menu.js')}}"></script>
-<script src="{{ asset('/js/main.js')}}"></script>
-<script src="{{ asset('/js/dashboards-analytics.js')}}"></script>
-
-<!-- Place this tag in your head or just before your close body tag. -->
-<script async defer src="{{ asset('https://buttons.github.io/buttons.js')}}"></script>
-
-
-
-{{-- <script src="{{ asset('\plugins\jquery-3.5.1.min.js')}}"></script> --}}
-<script src="{{ asset('plugins/datatables.net/jquery.dataTables.js') }}"></script>
-<script src="{{ asset('plugins/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
-<script src="{{ asset('plugins/magnific-popup/jquery.magnific-popup.min.js') }}"></script>
-<script src="{{ asset('plugins/toast/jquery.toast.min.js') }}"></script>
-<script src="{{ asset('plugins/jconfirm/jquery-confirm.min.js') }}"></script>
-<link href="{{asset('plugins/jconfirm/jquery-confirm.min.css')}}" rel="stylesheet">
-<script src="{{ asset('plugins/loadingoverlay.min.js') }}"></script>
-<script type="text/javascript" src="{{ asset('https://cdn.datatables.net/v/bs5/dt-1.11.4/datatables.min.js')}}"></script> 
-
-<script src="{{ asset('plugins/appEngine.js') }}" type="text/javascript"></script>
-<script type="text/javascript">
-    $appEngine.setHost("{{ url('/') }}");
-</script>
-
-<script src="{{ asset('/js/bootstrap.js')}}"></script>
-                    
-<!-- Page JS -->
-<script src="{{ asset('/js/pages-account-settings-account.js')}}"></script>
-
-<script src="{{ asset('/controllers/Pedidos.js')}}"></script>
- 
- 
-@yield('scripts')
-
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,24 @@
 <?php
- 
-use Illuminate\Support\Facades\Route; 
- 
-Route::get('/dashboard', function () { return view('dashboard'); })->middleware('auth');
 
-Route::get('/','\Modules\Auth\Controllers\LoginController@create')->name('login')->middleware('guest'); 
-Route::get('/resetPassword','\Modules\Auth\Controllers\LoginController@reset');
-Route::post('/login','\Modules\Auth\Controllers\LoginController@store');
-Route::post('/logout','\Modules\Auth\Controllers\LoginController@destroy');
-Route::post('/resetPassword', '\Modules\Auth\Controllers\LoginController@resetPassword')->middleware('guest')->name('password.reset');
-Route::get('/newPassword/{id}/{token}', '\Modules\Auth\Controllers\LoginController@newPasswordShow')->middleware('guest');
-Route::post('/newPassword', '\Modules\Auth\Controllers\LoginController@newPassword')->middleware('guest');
+use Illuminate\Support\Facades\Route;
 
-Route::get('/perfil','\Modules\Perfil\Controllers\PerfilController@show')->middleware('auth');
-Route::get('/cupones','\Modules\Cupones\Controllers\CuponesController@show')->middleware('auth');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::get('/monedero','\Modules\Monedero\Controllers\MonederoController@show')->middleware('auth');
-Route::get('/red','\Modules\Red\Controllers\RedController@show')->middleware('auth');
-
-
-Route::group(['prefix'=>'pedidos'], function () {
-    Route::get('/','\Modules\Pedidos\Controllers\PedidosController@show')->middleware('auth');
-    Route::get('/nuevoPedido','\Modules\Pedidos\Controllers\PedidosController@nuevoPedidoShow')->middleware('auth');
-
-    Route::get('/productos/{producto}','\Modules\Pedidos\Controllers\PedidosController@BuscadorProductos');
-    Route::get('/producto/{producto}','\Modules\Pedidos\Controllers\PedidosController@BuscadorProducto');
-    Route::post('/CrearPedido','\Modules\Pedidos\Controllers\PedidosController@CrearPedido');
-    Route::get('/getPedidos','\Modules\Pedidos\Controllers\PedidosController@getPedidos');
-    Route::post('/subirPago','\Modules\Pedidos\Controllers\PedidosController@subirPago');
-    Route::get('/verComprobante','\Modules\Pedidos\Controllers\PedidosController@verComprobante');
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
